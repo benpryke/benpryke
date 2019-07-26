e5cb0bf25c089dae9749108eafc1de453fad88bd
Ben Pryke <ninjakannon@hotmail.com>
2019-07-26 21:41:28 +0100

    Updates

---
+++ precache-manifest.d1ae1371e698b30f6ad6ca000e0f5904.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6b1fe604bab3e1579b1370dcb27981",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cd36c2dea4208c2ece0",
+    "url": "/static/css/main.059b175e.chunk.css"
+  },
+  {
+    "revision": "15aa850d3cd06c365e3c",
+    "url": "/static/js/2.48191205.chunk.js"
+  },
+  {
+    "revision": "6cd36c2dea4208c2ece0",
+    "url": "/static/js/main.197b2845.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
